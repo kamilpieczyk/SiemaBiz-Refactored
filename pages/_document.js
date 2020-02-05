8bc77ce25147bad841cb1b2ad3824409de61d606
kamil <kamil@coding-bear.co.uk>
2020-02-05 15:57:40 +0000

    Create project structure

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
-          {/* Step 5: Output the styles in the head  */}
+          <link rel='icon' href='/favicon.ico' />
           {this.props.styleTags}
         </Head>
         <body>
