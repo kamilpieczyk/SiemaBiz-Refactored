2f1d24d868e8412a32fcbe110d1d530714cdccb8
kamil <kamil@coding-bear.co.uk>
2020-03-30 11:32:16 +0100

    add categories array

---
+++ data/article-categories.js
@@ -0,0 +1,15 @@
+const categories = [
+  'top-ten-tips',
+  'videos',
+  'news',
+  'social-media',
+  'business',
+  'law',
+  'technology',
+  'tips',
+  'united-kingdom',
+  'poland',
+  'general'
+];
+
+export default categories
