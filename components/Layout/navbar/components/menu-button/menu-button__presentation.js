8a810e6d804fc80a43dbcc23010fed34e0fd87df
Kamil <kamil.pieczyk@gmail.com>
2020-06-17 12:55:27 +0100

    fix mobile menu link bug

--- components/Layout/navbar/components/menu-button/menu-button__presentation.js
+++ components/Layout/navbar/components/menu-button/menu-button__presentation.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import { Container, Burger, Text } from './menu-button__style'
-import { setMenuActive, setMenuInactive } from '../../../../../Redux/actions'
+import { Container, Burger, Text } from './menu-button__style';
+import { setMenuActive, setMenuInactive } from '../../../../../Redux/actions';
 
 const Presentation = ({ isClicked, handleClick, isMenuActive, dispatch }) => (
   <Container
-    onClick = { () => {
+    onClick={() => {
       handleClick();
-      if( isMenuActive ) dispatch( setMenuInactive() )
-      else dispatch( setMenuActive() )
-    } } 
+      if (isMenuActive) dispatch(setMenuInactive());
+      else dispatch(setMenuActive());
+    }}
   >
-    <Burger isClicked = { isClicked } />
+    <Burger isClicked={isMenuActive} />
     <Text>MENU</Text>
   </Container>
-)
+);
 
 Presentation.propTypes = {
   isClicked: PropTypes.bool.isRequired,
   handleClick: PropTypes.func.isRequired,
   isMenuActive: PropTypes.bool,
-  dispatch: PropTypes.func
-}
+  dispatch: PropTypes.func,
+};
 
-export default Presentation
+export default Presentation;
