6747cf38264d925828a9939450f4819f9577ed08
Kamil <kamil.pieczyk@gmail.com>
2020-06-17 13:16:21 +0100

    change burger menu appearance

--- components/Layout/navbar/components/menu-button/menu-button__presentation.js
+++ components/Layout/navbar/components/menu-button/menu-button__presentation.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import UseAnimation from 'react-useanimations';
 
 import { Container, Burger, Text } from './menu-button__style';
 import { setMenuActive, setMenuInactive } from '../../../../../Redux/actions';
+import Separator from '../../../../UI/separator';
 
 const Presentation = ({ isClicked, handleClick, isMenuActive, dispatch }) => (
   <Container
@@ -13,6 +15,8 @@ const Presentation = ({ isClicked, handleClick, isMenuActive, dispatch }) => (
     }}
   >
     <Burger isClicked={isMenuActive} />
+    <Separator width='10px' />
+    {/* <UseAnimation animationKey='menu' /> */}
     <Text>MENU</Text>
   </Container>
 );
