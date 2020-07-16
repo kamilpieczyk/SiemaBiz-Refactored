f02c69ce4bb58453d4057efd55c58157d1361f7f
Kamil <kamil.pieczyk@gmail.com>
2020-07-16 11:12:02 +0100

    ADD ANIMATIONS INTO WINDOW
    -animation on enter new window,
    -animation on close the window

--- components/UI/window/index.js
+++ components/UI/window/index.js
@@ -1,28 +1,53 @@
-import React from "react"
-import MaterialIcon from "@material/react-material-icon"
+import React, { useRef, useEffect } from 'react';
+import MaterialIcon from '@material/react-material-icon';
+import gsap from 'gsap';
 
-import { 
-  Container,
-  TopBar,
-  ContentWindow
- } from './window__styles'
+import { Container, TopBar, ContentWindow } from './window__styles';
 
-const Window = ( { close, width, height, children } ) => {
+const Window = ({ close, width, height, children }) => {
+  const containerRef = useRef(null);
+  const windowRef = useRef(null);
 
-  return(
-    <Container>
+  useEffect(() => {
+    const tl = gsap.timeline({ repeat: 0 });
+    tl.to(containerRef.current, {
+      duration: 0.4,
+      opacity: 1,
+    })
+      .to(windowRef.current, {
+        duration: 0.6,
+        height: 'auto',
+        maxHeight: '80%',
+      })
+      .to(windowRef.current, {
+        duration: 0.1,
+        overflow: 'auto',
+      });
+  }, []);
 
-      <TopBar width={ width } >
-        <MaterialIcon icon='close' onClick={ close } />
+  const handleCloseWindow = () => {
+    const tl = gsap.timeline({ repeat: 0 });
+    tl.to(windowRef.current, {
+      duration: 0.6,
+      height: 0,
+    })
+      .to(containerRef.current, {
+        duration: 0.4,
+        opacity: 0,
+      })
+      .then(() => close());
+  };
+
+  return (
+    <Container ref={containerRef}>
+      <TopBar width={width}>
+        <MaterialIcon icon='close' onClick={handleCloseWindow} />
       </TopBar>
-
-      <ContentWindow width={ width } height={ height }>
-        { children }
+      <ContentWindow width={width} height={height} ref={windowRef}>
+        {children}
       </ContentWindow>
-
     </Container>
-  )
-
-}
+  );
+};
 
 export default Window;
--- components/UI/window/window__styles.js
+++ components/UI/window/window__styles.js
@@ -1,63 +1,64 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
-import colors from '../../../styles/colors'
-
+import colors from '../../../styles/colors';
 
 export const Container = styled.div`
-    z-index: 2000;
-    position: fixed;
-    left: 0;
-    top: 0;
-    background: ${ colors.transparentBlack };
-    width: 100%;
-    height: 100%;
-    backdrop-filter: blur( 10px );
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+  z-index: 2000;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background: ${colors.transparentWhite};
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(10px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+`;
 
 export const TopBar = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    width: ${ ( { width } ) => width };
-    padding: 10px;
-    background: ${ colors.main };
-    border-radius: 5px 5px 0 0;
-    i{
-        cursor: pointer;
-        color: ${ colors.white };
-    }
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: ${({ width }) => width};
+  padding: 10px;
+  background: ${colors.main};
+  border-radius: 5px 5px 0 0;
+  i {
+    cursor: pointer;
+    color: ${colors.white};
+  }
 
-    @media ( max-width: 800px ) {
-        width: 95%;
-    }
-`
+  @media (max-width: 800px) {
+    width: 95%;
+  }
+`;
 
 export const ContentWindow = styled.div`
-    width: ${ ( { width } ) => width };
-    ${ ( { height } ) => height ? `height: ${ height }` : null };
-    ${ ( { height } ) => height ? `max-height: ${ height }` : "80vh" };
-    background: ${ colors.white };
-    border-radius: 0 0 5px 5px;
-    padding: 20px;
-    overflow: auto;
-    position: relative;
+  width: ${({ width }) => width};
+  height: 0;
+  ${({ height }) => (height ? `height: ${height}` : null)};
+  ${({ height }) => (height ? `max-height: ${height}` : '80vh')};
+  background: ${colors.white};
+  border-radius: 0 0 5px 5px;
+  padding: 20px;
+  overflow: hidden;
+  position: relative;
 
-    ::-webkit-scrollbar{
-        width: 5px;
-        background-color: ${ colors.transparentBlack };
-        backdrop-filter: blur(5px);
-    }
-    ::-webkit-scrollbar-thumb{
-        background-color: ${ colors.main };
-        border-radius: 2px;
-    }
+  ::-webkit-scrollbar {
+    width: 5px;
+    background-color: ${colors.transparentBlack};
+    backdrop-filter: blur(5px);
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${colors.main};
+    border-radius: 2px;
+  }
 
-    @media ( max-width: 800px ) {
-        width: 95%;
-        height: 90%;
-    }
-`
+  @media (max-width: 800px) {
+    width: 95%;
+    height: 90%;
+  }
+`;
