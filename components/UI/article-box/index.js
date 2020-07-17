a1729b7d6ffd5367d2aa1d685cff1b5b2cf9b5a8
Kamil <kamil.pieczyk@gmail.com>
2020-07-17 10:46:55 +0100

    add new link handle into article box

--- components/UI/article-box/index.js
+++ components/UI/article-box/index.js
@@ -1,50 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import Link from 'next/link'
-import { useSelector } from 'react-redux'
-import MaterialIcon from '@material/react-material-icon'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+// import Link from 'next/link'
+import { useSelector } from 'react-redux';
+import MaterialIcon from '@material/react-material-icon';
 
-import { Container, Image, Content, ReadArticle, AuthorAndDate } from './article-box__styles'
-import apiKey from '../../../API/key'
+import { Container, Image, Content, ReadArticle, AuthorAndDate } from './article-box__styles';
+import apiKey from '../../../API/key';
+import Link from '../../../API/link';
 
 const ArticleBox = ({ article }) => {
-  
-  const [ imageWidth, setImageWidth ] = useState( 170 );
-  const language = useSelector( s => s.language.source );
+  const [imageWidth, setImageWidth] = useState(170);
+  const language = useSelector(s => s.language.source);
   const introduction = article.introduction.split(' ');
 
-  useEffect(
-    () => {
-      const imageWidth = document.getElementById('article_image').offsetWidth;
-      setImageWidth( imageWidth );
-    }, []
-  )
+  useEffect(() => {
+    const imageWidth = document.getElementById('article_image').offsetWidth;
+    setImageWidth(imageWidth);
+  }, []);
 
-  return(
+  return (
     <Container>
-      <Image id = 'article_image' imageWidth = { imageWidth } src = { `${ apiKey }uploads/images/${ article.image }` } />
+      <Image id='article_image' imageWidth={imageWidth} src={`${apiKey}uploads/images/${article.image}`} />
       <Content>
-        <h2>{ article.title }</h2>
+        <h2>{article.title}</h2>
         <AuthorAndDate>
-          <h4>{ article.category }</h4>
-          <p>{ article.date }</p>
-          <h3><MaterialIcon icon = 'account_circle'/>{ article.author }</h3>
+          <h4>{article.category}</h4>
+          <p>{article.date}</p>
+          <h3>
+            <MaterialIcon icon='account_circle' />
+            {article.author}
+          </h3>
         </AuthorAndDate>
         <p>
-          { introduction.slice( 0, 60 ).join(' ') + ' ' }
-          <Link href = { { pathname: 'article', query: { id: article._id } } }>
-            <a>...{ language.articlesPage.button }</a>
+          {introduction.slice(0, 60).join(' ') + ' '}
+          <Link adress={{ pathname: '/article', query: { id: article._id } }}>
+            <a>...{language.articlesPage.button}</a>
           </Link>
         </p>
         <ReadArticle>
-          <Link href = { { pathname: 'article', query: { id: article._id } } }>
-            <a>{ language.articlesPage.button }</a>
+          <Link adress={{ pathname: '/article', query: { id: article._id } }}>
+            <a>{language.articlesPage.button}</a>
           </Link>
         </ReadArticle>
       </Content>
     </Container>
-  )
-}
+  );
+};
 
 ArticleBox.propTypes = {
   article: PropTypes.shape({
@@ -54,7 +55,7 @@ ArticleBox.propTypes = {
     introduction: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
-  })
-}
+  }),
+};
 
 export default ArticleBox;
