fa8f94eb78f75d2b94b939f09b9de11972957a65
Kamil <kamil.pieczyk@gmail.com>
2020-07-17 12:46:25 +0100

    fix admin panel privileges bug in sidebar

--- data/admin-panel-menu.js
+++ data/admin-panel-menu.js
@@ -9,21 +9,25 @@ export const getAdminPanelMenu = () => {
       title: language.articleList,
       icon: 'list',
       href: '/user-panel/administration-panel?page=articles',
+      priv: 225803,
     },
     {
       title: language.companyManagement,
       icon: 'business_center',
       href: '/user-panel/administration-panel?page=company',
+      priv: 225801,
     },
     {
       title: language.apoitments,
       icon: 'people',
       href: '/user-panel/administration-panel?page=apoitments',
+      priv: 225805,
     },
     {
       title: language.adminPanel,
       icon: 'admin_panel_settings',
       href: '/user-panel/administration-panel?page=admin',
+      priv: 225805,
     },
   ];
 
