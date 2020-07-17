bff71e20239071724314bc96df0e4b70512857d0
Kamil <kamil.pieczyk@gmail.com>
2020-07-17 10:56:10 +0100

    add new link into sidebar hiperlinks

--- components/UI/sidebar-box/menu-item/index.js
+++ components/UI/sidebar-box/menu-item/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import Link from 'next/link'
-import PropTypes from 'prop-types'
-import Icon from '@material/react-material-icon'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from '@material/react-material-icon';
 
-import { Container } from './menu-item__styles'
+import { Container } from './menu-item__styles';
+import Link from '../../../../API/link';
 
 const MenuItem = ({ icon, href, children, light }) => (
-  <Link href = { href }>
-    <Container light = { light }>
-      <Icon icon = { icon } />
-      <a>{ children }</a>
+  <Link adress={`/${href}`}>
+    <Container light={light}>
+      <Icon icon={icon} />
+      <a>{children}</a>
     </Container>
   </Link>
-)
+);
 
 MenuItem.propTypes = {
   icon: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired
-}
+  children: PropTypes.string.isRequired,
+};
 
-export default MenuItem
+export default MenuItem;
