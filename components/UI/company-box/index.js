329506124845b5bd1f4ac32196ea33d5694ddad1
Kamil <kamil.pieczyk@gmail.com>
2020-07-17 10:51:43 +0100

    add new link handle into company box

--- components/UI/company-box/index.js
+++ components/UI/company-box/index.js
@@ -7,17 +7,19 @@ import { Container } from './company-box__styles';
 import ApiKey from '../../../API/key';
 import Button from '../small-button';
 import withClick from '../../HOC/withClick';
+import { useLink } from '../../../API/link';
 
 const ClickableButton = withClick(Button);
 
 const CompanyBox = ({ company }) => {
   const language = useSelector(s => s.language.source);
   const router = useRouter();
+  const link = useLink();
 
   const description = company.description.split(' ').slice(0, 60).join(' ');
 
   const handleButton = id => {
-    router.push({
+    link({
       pathname: '/company',
       query: { id },
     });
