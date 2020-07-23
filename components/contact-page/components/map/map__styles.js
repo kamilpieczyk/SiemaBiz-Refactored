6298bbfd42b2a3b76b51fa849a4d9982f438051d
Kamil <kamil.pieczyk@gmail.com>
2020-07-23 19:21:16 +0100

    add final version of Map component in contact page

--- components/contact-page/components/map/map__styles.js
+++ components/contact-page/components/map/map__styles.js
@@ -10,11 +10,21 @@ export const Container = styled.div`
   display: flex;
   padding-top: 100px;
 
+  @media (max-width: ${mobile}px) {
+    flex-direction: column;
+    height: 120vh;
+  }
+
   iframe {
     flex: 1;
     margin: 0;
     width: 50%;
     height: 100%;
+
+    @media (max-width: ${mobile}px) {
+      width: 100%;
+      height: 50%;
+    }
   }
 `;
 
@@ -28,6 +38,7 @@ export const Info = styled.div`
   height: 100%;
   background: ${colors.main};
   color: ${colors.white};
+  text-align: center;
   div {
     display: flex;
     align-items: center;
@@ -36,5 +47,10 @@ export const Info = styled.div`
       bottom: 3px;
       right: 5px;
     }
+  }
+  @media (max-width: ${mobile}px) {
+    width: 100%;
+    height: 50%;
+    padding: 10px 0;
   }
 `;
