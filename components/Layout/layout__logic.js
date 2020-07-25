f7460091285a3e20a5510a58e3b42bd7873771bb
Kamil <kamil.pieczyk@gmail.com>
2020-07-25 18:03:16 +0100

    fix resolution detection bug

--- components/Layout/layout__logic.js
+++ components/Layout/layout__logic.js
@@ -1,63 +1,58 @@
-import { PureComponent } from 'react'
-import { connect } from 'react-redux'
+import { Component } from 'react';
+import { connect } from 'react-redux';
 
 import {
   loginUser,
   changeLanguageToENG,
   changeLanguageToPL,
   setScreenResolutionToMobile,
-  setScreenResolutionToDesktop
-} from '../../Redux/actions'
-import { mobile } from '../../styles/devices'
+  setScreenResolutionToDesktop,
+} from '../../Redux/actions';
+import { mobile } from '../../styles/devices';
 
-class LayoutLogic extends PureComponent{
-
+class LayoutLogic extends Component {
   checkDeviceScreenResolution = () => {
     const mobileResolution = mobile;
-    let windowResolution = window.innerWidth;
-    if( windowResolution <= mobileResolution ) this.props.setScreenResolutionToMobile()
-    else this.props.setScreenResolutionToDesktop()    
-  }
+    let windowResolution = window.screen.availWidth;
+    if (windowResolution <= mobileResolution) this.props.setScreenResolutionToMobile();
+    else this.props.setScreenResolutionToDesktop();
+  };
 
   checkBrowserLanguage = () => {
     const browserLanguage = navigator.language;
 
-    if( browserLanguage === 'pl-PL' ){
+    if (browserLanguage === 'pl-PL') {
       this.props.setLanguageToPL();
-    }
-    else{
+    } else {
       this.props.setLanguageToEN();
     }
+  };
 
-  }
-
-  componentDidMount(){
+  componentDidMount() {
     this.checkBrowserLanguage();
     this.checkDeviceScreenResolution();
-    window.addEventListener( 'resize', this.checkDeviceScreenResolution );
+    window.addEventListener('resize', this.checkDeviceScreenResolution);
   }
 
-  componentWillUnmount(){
-    window.removeEventListener( 'resize', this.checkDeviceScreenResolution );
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.checkDeviceScreenResolution);
   }
-  
-  render(){
-    return this.props.render({
 
-    })
+  render() {
+    return this.props.render({});
   }
 }
 
 export default connect(
   state => ({
     user: state.user,
-    deviceScreen: state.deviceScreen
+    deviceScreen: state.deviceScreen,
   }),
   dispatch => ({
-    login: user => dispatch( loginUser( user ) ),
-    setLanguageToPL: () => dispatch( changeLanguageToPL() ),
-    setLanguageToEN: () => dispatch( changeLanguageToENG() ),
-    setScreenResolutionToMobile: () => dispatch( setScreenResolutionToMobile() ),
-    setScreenResolutionToDesktop: () => dispatch( setScreenResolutionToDesktop() ),
+    login: user => dispatch(loginUser(user)),
+    setLanguageToPL: () => dispatch(changeLanguageToPL()),
+    setLanguageToEN: () => dispatch(changeLanguageToENG()),
+    setScreenResolutionToMobile: () => dispatch(setScreenResolutionToMobile()),
+    setScreenResolutionToDesktop: () => dispatch(setScreenResolutionToDesktop()),
   })
-)(LayoutLogic)
+)(LayoutLogic);
