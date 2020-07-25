33a4690d755c1dfc919258b5c94f20da5699ebbe
Kamil <kamil.pieczyk@gmail.com>
2020-07-25 17:36:42 +0100

    change api key file
    set api key as function which returns api key

--- API/key.js
+++ API/key.js
@@ -1,1 +1,8 @@
-export default 'http://localhost:5002/';
+const key = () => {
+  const protocol = 'http:';
+  const host = 'localhost';
+  const port = 5002;
+  return `${protocol}//${host}:${port}/`;
+};
+
+export default key();
