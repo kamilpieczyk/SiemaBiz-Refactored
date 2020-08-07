cafed8edb00a899802dca210736e5c3236491289
Kamil <kamil.pieczyk@gmail.com>
2020-08-07 16:46:36 +0100

    fix parallax effect on home page

--- components/index-page/components/search-box/searchBox__presentation.js
+++ components/index-page/components/search-box/searchBox__presentation.js
@@ -1,52 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux'
-import MaterialIcon from '@material/react-material-icon'
-import { debounce } from 'lodash'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import MaterialIcon from '@material/react-material-icon';
+import { debounce } from 'lodash';
 
-import { 
-  Container,
-  Box,
-  Input
-} from './searchBox__styles'
+import { Container, Box, Input } from './searchBox__styles';
 
 const SearchBoxPresentationLayer = ({ state, handlers, inputRef, small }) => {
-
-  const language = useSelector( s => s.language.source );
+  const language = useSelector(s => s.language.source);
 
   const router = useRouter();
   const searchQuery = router.query.search;
 
-  return(
-    <Container small = { small }>
-      <Box small = { small }>
-        { !small && <h1>{ language.indexPage.searchBoxTitle }</h1> }
-        <Input small = { small }>
-          <div className = 'icon'>
-            <MaterialIcon icon = 'search' />
+  return (
+    <Container small={small} opacity={state.backgroundOpacity} movement={state.backgroundPositionMovement}>
+      <Box small={small}>
+        {!small && <h1>{language.indexPage.searchBoxTitle}</h1>}
+        <Input small={small}>
+          <div className='icon'>
+            <MaterialIcon icon='search' />
           </div>
           <input
-            type = 'text'
-            ref = { inputRef }
-            defaultValue = { searchQuery }
-            onChange = { debounce( handlers.search, 1000 ) }
+            type='text'
+            ref={inputRef}
+            defaultValue={searchQuery}
+            onChange={debounce(handlers.search, 1000)}
           />
         </Input>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
 SearchBoxPresentationLayer.propTypes = {
   state: PropTypes.shape({
-    searchValue: PropTypes.string
+    searchValue: PropTypes.string,
   }),
   handlers: PropTypes.shape({
     search: PropTypes.func.isRequired,
   }),
   inputRef: PropTypes.object.isRequired,
-  small: PropTypes.bool
-}
+  small: PropTypes.bool,
+};
 
 export default SearchBoxPresentationLayer;
--- components/index-page/components/search-box/searchBox__styles.js
+++ components/index-page/components/search-box/searchBox__styles.js
@@ -1,65 +1,67 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-import colors from '../../../../styles/colors'
-import { mobile } from '../../../../styles/devices'
-import { backdropFilter, absoluteCenter, flexCenter } from '../../../../styles/mixins'
+import colors from '../../../../styles/colors';
+import { mobile } from '../../../../styles/devices';
+import { backdropFilter, absoluteCenter, flexCenter } from '../../../../styles/mixins';
 
 export const Container = styled.div`
   width: 100vw;
-  height: ${ ({ small }) => small ? '200px' : '100vh' };
-  background-size: ${ ({ small }) => small ? '100% 100vh' : '100% 100%' };
+  height: ${({ small }) => (small ? '200px' : '100vh')};
+  background-size: ${({ small }) => (small ? '100% 100vh' : 'cover')};
   background-repeat: no-repeat;
-  background-position: center;
-  background-image: ${ ({ small }) => small ? 'none' : 'url( images/index-desktop.png )' };
+  background-position: ${({ movement }) => `center -${movement / 3}px`};
+  background-image: ${({ small }) => (small ? 'none' : 'url( images/index-desktop.png )')};
+  background-attachment: fixed;
   position: relative;
+  opacity: ${({ opacity }) => opacity};
 
-  @media ( max-width: ${ mobile }px ) {
-    background-size: ${ ({ small }) => small ? '400% 100vh' : '400% 100%' };
+  @media (max-width: ${mobile}px) {
+    background-size: ${({ small }) => (small ? '400% 100vh' : '400% 100%')};
     background-position-x: 80%;
-    height: ${ ({ small }) => small ? '30vh' : '70vh' };
+    height: ${({ small }) => (small ? '30vh' : '70vh')};
   }
-`
+`;
 
 export const Box = styled.div`
-  ${ absoluteCenter() }
+  ${absoluteCenter()}
   padding: 40px 30px;
   border-radius: 5px;
-  ${ ({ small }) => small && 'top: 70%;' }
+  ${({ small }) => small && 'top: 70%;'}
 
-  h1{
-    color: ${ ({ small }) =>  small ? colors.main : colors.white };
-    text-shadow: 0px 0px 3px ${ colors.black };
+  h1 {
+    color: ${({ small }) => (small ? colors.main : colors.white)};
+    text-shadow: 0px 0px 3px ${colors.black};
     font-weight: 100;
     font-size: 2rem;
     text-align: center;
   }
-`
+`;
 
 export const Input = styled.div`
   width: 50vw;
   height: 40px;
   display: flex;
-  @media ( max-width: ${ mobile }px ) {
+  @media (max-width: ${mobile}px) {
     width: 90vw;
   }
 
-  .icon{
-    background: ${ colors.main };
-    color: ${ colors.white };
+  .icon {
+    background: ${colors.main};
+    color: ${colors.white};
     border-radius: 5px 0 0 5px;
     flex: 1;
-    ${ flexCenter() };
-    ${ ({ small }) => small && `box-shadow: 1px 1px 5px ${ colors.lightGrey };` }
+    ${flexCenter()};
+    ${({ small }) => small && `box-shadow: 1px 1px 5px ${colors.lightGrey};`}
   }
 
-  input{
+  input {
     border: none;
     text-align: center;
-    background: ${ ({ small }) => small ? colors.ultraWhite : colors.white };
+    background: ${({ small }) => (small ? colors.ultraWhite : colors.white)};
     height: 100%;
     flex: 14;
     border-radius: 0 5px 5px 0;
-    caret-color: ${ colors.main };
-    ${ ({ small }) => small && `box-shadow: 1px 1px 5px ${ colors.lightGrey };` }
+    caret-color: ${colors.main};
+    ${({ small }) => small && `box-shadow: 1px 1px 5px ${colors.lightGrey};`}
   }
-`
+`;
