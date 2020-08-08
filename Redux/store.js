0a967683a753a15f1a4c4ed02d9dc6af76b8d755
Kamil <kamil.pieczyk@gmail.com>
2020-08-08 14:14:33 +0100

    create redux production store

--- Redux/store.js
+++ Redux/store.js
@@ -2,19 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './Reducers';
 import thunk from 'redux-thunk';
 
-let store;
-const isFirefox = typeof InstallTrigger !== 'undefined';
-
-if (process.browser && !isFirefox) {
-  store = createStore(
-    reducers,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else {
-  store = createStore(reducers, applyMiddleware(thunk));
-}
+let store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
