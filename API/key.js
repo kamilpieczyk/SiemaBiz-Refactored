ab53be580ab8d3d04d7dd13077c5da7420345ec7
Kamil Pieczyk <45919756+kamilpieczyk@users.noreply.github.com>
2022-04-10 16:49:23 +0100

    switch to https

--- API/key.js
+++ API/key.js
@@ -2,7 +2,7 @@ const key = () => {
   const protocol = 'https:';
   const host = '18.218.167.24';
   const port = 5002;
-  return `${protocol}//${host}:${port}/`;
+  return `${protocol}//${host}/`;
 };
 
 export default key();
